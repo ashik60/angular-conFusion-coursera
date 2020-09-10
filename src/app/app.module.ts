9482ceacd442a5c9daf1eedaacb545cdfb970b0e
ashiksh <ashikshaheen187@gmail.com>
2020-09-10 13:20:01 +0600

    spa p1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,14 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
 
 @NgModule({
   declarations: [
@@ -21,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +42,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
